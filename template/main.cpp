0e9b75e9d6bef492426186198b644ae1a95750df
poyo-poyo <ik0390@gmail.com>
2018-06-10 17:30:35 +0900

    テンプレ追加

---
+++ template/main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+#define rep(i,n) for(int i=0;i<(n);i++)
+
+int main(){
+
+  
+
+  return 0;
+}
---
+++ template/main.d
@@ -0,0 +1,12 @@
+void main(){
+  import std.stdio, std.string, std.conv, std.algorithm;
+
+  
+}
+
+void rd(T...)(ref T x){
+  import std.stdio, std.string, std.conv;
+  auto l=readln.split;
+  assert(l.length==x.length);
+  foreach(i, ref e; x) e=l[i].to!(typeof(e));
+}

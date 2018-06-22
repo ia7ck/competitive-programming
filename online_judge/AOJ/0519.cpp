3cfdc902944ba74ecc22f93252f3395f0952642f
poyo-poyo <ik0390@gmail.com>
2018-06-23 01:21:30 +0900

    solve 0519

---
+++ online_judge/AOJ/0519.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+#define rep(i,n) for(int i=0;i<(n);i++)
+
+int main(){
+
+  int n, m; cin>> n>> m;
+  vector<vector<int>> g(n);
+  rep(_, m){
+    int a, b; cin>> a>> b;
+    g[a-1].push_back(b-1);
+  }
+
+  vector<int> deg_in(n, 0);
+  rep(i, n)for(int j: g[i]) deg_in[j]++;
+  queue<int> Q;
+  rep(i, n)if(deg_in[i]==0) Q.push(i);
+  vector<int> order;
+  bool multi=false;
+  while(Q.size()>0){
+    auto i=Q.front(); Q.pop();
+    order.push_back(i);
+    int cnt=0;
+    for(int j: g[i]){
+      if((--deg_in[j])==0){
+        Q.push(j);
+        cnt++;
+      }
+    }
+    if(cnt>=2) multi=true;
+  }
+
+  for(int i: order) cout<< i+1<< endl;
+  if(multi) cout<< 1<< endl;
+  else cout<< 0<< endl;
+
+  return 0;
+}

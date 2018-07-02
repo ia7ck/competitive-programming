9ff588bf52266bed2d91f6f018e837dabdaa71c1
poyo-poyo <ik0390@gmail.com>
2018-07-02 19:04:46 +0900

    update

--- online_judge/yukicoder/704.cpp
+++ online_judge/yukicoder/704.cpp
@@ -7,17 +7,17 @@ using namespace std;
 
 struct SegmentTree{
   int n=1, inf=1e8;
-  vector<int> seg;
+  vector<int> dat;
   SegmentTree(int m){
     while(n<m) n*=2;
-    seg.resize(n*2-1, inf);
+    dat.resize(n*2-1, inf);
   }
   void update(int i, int x){
     i+=n-1;
-    seg[i]=x;
+    dat[i]=x;
     while(i>0){
       i=(i-1)/2;
-      seg[i]=min(seg[i*2+1], seg[i*2+2]);
+      dat[i]=min(dat[i*2+1], dat[i*2+2]);
     }
   }
   int getmin(int ql, int qr){
@@ -25,7 +25,7 @@ struct SegmentTree{
   }
   int getmin(int ql, int qr, int i, int il, int ir){
     if(qr<=il || ir<=ql) return inf;
-    if(ql<=il && ir<=qr) return seg[i];
+    if(ql<=il && ir<=qr) return dat[i];
     int m=(ir+il)/2;
     int vl=getmin(ql, qr, i*2+1, il, m),
         vr=getmin(ql, qr, i*2+2, m, ir);
@@ -46,22 +46,19 @@ int main(){
   rep(i, n) cin>> y[i+1];
 
   const int inf=1e8;
-  SegmentTree seg_l(n), seg_r(n);
+  SegmentTree segt(n);
 
   vector<int> dp(n+1, inf);
   dp[0]=0;
   for(int i=1, j=1; i<=n; i++){
-    seg_r.update(i-1, x[i]+y[i]+dp[i-1]);
+    segt.update(i-1, x[i]+y[i]+dp[i-1]);
     while(j<=i && x[j]<=a[i]){
-      seg_l.update(j-1, -x[j]+y[j]+dp[j-1]);
-      seg_r.update(j-1, inf);
+      segt.update(j-1, -x[j]+y[j]+dp[j-1]);
       j++;
     }
-    dp[i]=dp[i-1]+abs(a[i]-x[i])+y[i];
-    chmin(dp[i], seg_l.getmin(0, i)+a[i]);
-    chmin(dp[i], seg_r.getmin(0, i)-a[i]);
+    chmin(dp[i], segt.getmin(0, j-1)+a[i]);
+    chmin(dp[i], segt.getmin(j-1, i)-a[i]);
   }
-
   cout<< dp[n]<< endl;
 
   return 0;

4a8ebbcf05c9e15af7e4dcf6c49e03fad802b655
ia7ck <ik0390@gmail.com>
2018-09-20 22:48:03 +0900

    format

--- online_judge/CSAcademy/Round42/sorting-steps.cpp
+++ online_judge/CSAcademy/Round42/sorting-steps.cpp
@@ -1,52 +1,61 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-#include<map>
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <vector>
 
 using namespace std;
-#define rep(i, n) for(int i=0; i<(int)(n); i++)
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
 
-struct FenwickTree{
+struct FenwickTree {
   int n;
   vector<int> dat;
-  FenwickTree(int n):n(n){
-    dat.resize(n+1, 0); // [1, n]を使う
+  FenwickTree(int n) : n(n) {
+    dat.resize(n + 1, 0); // [1, n]を使う
   }
-  void add(int i, int x){
-    if(i<=0) return;
-    for(int k=i; k<=n; k+=k&(-k)) dat[k]+=x;
+  void add(int i, int x) {
+    if (i <= 0)
+      return;
+    for (int k = i; k <= n; k += k & (-k))
+      dat[k] += x;
   }
-  int sum(int i){ // [1, i]の和
-    if(i<=0) return 0;
-    int s=0;
-    for(int k=i; k>0; k-=k&(-k)) s+=dat[k];
+  int sum(int i) { // [1, i]の和
+    if (i <= 0)
+      return 0;
+    int s = 0;
+    for (int k = i; k > 0; k -= k & (-k))
+      s += dat[k];
     return s;
   }
-  int sum(int i, int j){ // [i, j]の和 (i<=j)
-    if(i>j) return 0;
-    else return sum(j)-sum(i-1);
+  int sum(int i, int j) { // [i, j]の和 (i<=j)
+    if (i > j)
+      return 0;
+    else
+      return sum(j) - sum(i - 1);
   }
 };
 
-int main(){
+int main() {
 
-  int n; cin>> n;
+  int n;
+  cin >> n;
   vector<int> a(n);
-  rep(i, n) cin>> a[i];
+  rep(i, n) cin >> a[i];
 
-  int tot=0;
+  int tot = 0;
   FenwickTree tree(a.size()); // dat[i]:=a[i]を見たか
   vector<pair<int, int>> ai;
   rep(i, n) ai.push_back({a[i], i});
-  sort(ai.begin(), ai.end(), [&](const auto &l, const auto &r){
-      if(l.first==r.first) return l.second>r.second;
-      else return l.first>r.first;
+  sort(ai.begin(), ai.end(), [&](const auto &l, const auto &r) {
+    if (l.first == r.first)
+      return l.second > r.second;
+    else
+      return l.first > r.first;
   });
-  for(auto pr: ai){
-    tot=max(tot, tree.sum(1, pr.second));
-    tree.add(pr.second+1, 1);
+  for (auto pr : ai) {
+    tot = max(tot, tree.sum(1, pr.second));
+    tree.add(pr.second + 1, 1);
   }
-  cout<< tot+1<< endl;
+  cout << tot + 1 << endl;
 
   return 0;
 }

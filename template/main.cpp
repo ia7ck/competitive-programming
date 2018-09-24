1501bc3f907a7d88abd7e7eacaea01e588b7baf8
ia7ck <ik0390@gmail.com>
2018-09-24 21:24:11 +0900

    format

--- template/main.cpp
+++ template/main.cpp
@@ -1,13 +1,12 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-#define rep(i,n) for(int i=0;i<(n);i++)
+#define rep(i, n) for (int i = 0; i < (n); i++)
 
-int main(){
-
-  
+int main() {
+  //
 
   return 0;
 }
--- template/main.d
+++ template/main.d
@@ -1,12 +1,15 @@
-void main(){
+void main() {
   import std.stdio, std.string, std.conv, std.algorithm;
 
-  
 }
 
-void rd(T...)(ref T x){
-  import std.stdio, std.string, std.conv;
-  auto l=readln.split;
-  assert(l.length==x.length);
-  foreach(i, ref e; x) e=l[i].to!(typeof(e));
+void rd(T...)(ref T x) {
+  import std.stdio : readln;
+  import std.string : split;
+  import std.conv : to;
+
+  auto l = readln.split;
+  assert(l.length == x.length);
+  foreach (i, ref e; x)
+    e = l[i].to!(typeof(e));
 }

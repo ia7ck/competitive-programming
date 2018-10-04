cf12265c0c2ac3ebae6bb73219c1e6a5bdc5f1ef
ia7ck <ik0390@gmail.com>
2018-10-04 18:07:17 +0900

    ECR050

--- online_judge/Codeforces/ECR050/C/c.d
+++ online_judge/Codeforces/ECR050/C/c.d
@@ -6,40 +6,43 @@ void main() {
   rd(t);
   while (t--) {
     auto args = readln.split.to!(long[]);
-    long solve(long s) {
-      int[] n;
-      do {
-        n = s % 10 ~ n;
-        s /= 10;
-      }
-      while (s);
-      auto memo = new long[][][](n.length + 1, 2, n.length + 1);
-      foreach (i; 0 .. (n.length + 1))
-        foreach (j; 0 .. 2)
-          foreach (k; 0 .. (n.length + 1))
-            memo[i][j][k] = -1;
-      long f(size_t i, bool less, int count) {
-        if (i == n.length) {
-          return count <= 3;
-        } else {
-          if (memo[i][less][count] >= 0)
-            return memo[i][less][count];
-          long ret = 0;
-          auto digit = less ? 9 : n[i];
-          for (int d = 0; d <= digit; d++) {
-            auto l = less || (d < digit);
-            auto c = count + (d > 0);
-            ret += f(i + 1, l, c);
+    long solve(long nn) {
+      auto n = digit(nn);
+      auto dp = new long[][][](n.length + 1, 2, 20);
+      dp[0][0][0] = 1;
+      foreach (i; 0 .. n.length) {
+        foreach (l; 0 .. 2) {
+          foreach (c; 0 .. 19) {
+            auto d = l ? 9 : n[i];
+            for (int digit = 0; digit <= d; digit++) {
+              auto less = l || (digit < d);
+              auto count = c + (digit > 0);
+              dp[i + 1][less][count] += dp[i][l][c];
+            }
           }
-          return memo[i][less][count] = ret;
         }
       }
-
-      return f(0, 0, 0);
+      long ret = 0;
+      foreach (l; 0 .. 2) {
+        for (int c = 0; c <= 3; c++) {
+          ret += dp[n.length][l][c];
+        }
+      }
+      return ret;
     }
 
     writeln(solve(args[1]) - solve(args[0] - 1));
   }
+}
+
+int[] digit(long x) {
+  int[] ret;
+  do {
+    ret = x % 10 ~ ret;
+    x /= 10;
+  }
+  while (x);
+  return ret;
 }
 
 void rd(T...)(ref T x) {

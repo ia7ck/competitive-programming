3b911d527c7bc1c41e9073c9a8fd68d74f1b7cda
ia7ck <ik0390@gmail.com>
2018-12-09 16:41:25 +0900

    add comment

--- online_judge/yukicoder/n761.nim
+++ online_judge/yukicoder/n761.nim
@@ -1,22 +1,22 @@
 import strutils, sequtils
 
-proc f(a: seq[int64], turn: bool): bool =
+proc f(a: seq[int64]): bool = # aを受け取った人は必勝？
   let n = a.len
   if a.all(proc(x: int64): bool = a[0]==x):
-    result = true
+    result = true             # 残り全部取れるから勝ち
   else:
     let s = a.foldl(a+b)
     var j = 0
     while j<n and a[j]*n<s:
       j+=1
     let
-      res1 = f(a[0..<j], not turn)
-      res2 = f(a[j..<n], not turn)
-    result = not (res1 and res2)
+      res1 = f(a[0..<j])
+      res2 = f(a[j..<n])
+    result = not (res1 and res2) # 相手が必勝&必勝だったら負け
 
 proc main() =
   let
     n = stdin.readLine.parseInt
     a = stdin.readLine.split.map(parseBiggestInt)
-  echo if f(a, true): "First" else: "Second"
+  echo if f(a): "First" else: "Second"
 main()

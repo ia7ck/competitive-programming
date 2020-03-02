39e037f35fbf7b44362e6e05b58913b012f13bc9
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-03-02 23:38:01 +0900

    SegmentTree

--- library/cpp/segment_tree.cpp
+++ library/cpp/segment_tree.cpp
@@ -1,36 +1,24 @@
 #include <algorithm>
 #include <cassert>
+#include <functional>
 #include <iostream>
 #include <vector>
 
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 using namespace std;
 
-struct Monoid {
-  int64_t val;
-  Monoid() {}
-  Monoid(int64_t v) : val(v) {}
-  static Monoid ident() { return Monoid(0); }
-  static Monoid multiply(Monoid l, Monoid r) { return Monoid(l.val + r.val); }
-};
-
 template <typename M>
 struct SegmentTree {
   int n;
-  M e;
   vector<M> dat;
-  SegmentTree(int n0, vector<M> a) {
-    assert(n0 == (int)a.size());
+  M e;
+  function<M(M, M)> multiply;
+  SegmentTree(int n0, M e, function<M(M, M)> f) : e(e), multiply(f) {
     n = 1;
-    e = M::ident();
     while (n < n0) {
-      n *= 2;
+      n = n * 2;
     }
     dat.resize(n * 2 - 1, e);
-    rep(i, n0) dat[i + n - 1] = a[i];
-    for (int i = n - 2; i >= 0; i--) {
-      dat[i] = M::multiply(dat[i * 2 + 1], dat[i * 2 + 2]);
-    }
   }
   M get(int i) { return dat[i + n - 1]; }
   void update(int i, M m) {
@@ -38,7 +26,7 @@ struct SegmentTree {
     dat[k] = m;
     while (k) {
       k = (k - 1) / 2;
-      dat[k] = M::multiply(dat[k * 2 + 1], dat[k * 2 + 2]);
+      dat[k] = multiply(dat[k * 2 + 1], dat[k * 2 + 2]);
     }
   }
   M query(int ql, int qr) { return query(ql, qr, 0, 0, n); }
@@ -49,8 +37,8 @@ struct SegmentTree {
       return e;
     } else {
       auto m = (il + ir) / 2;
-      return M::multiply(query(ql, qr, i * 2 + 1, il, m),
-                         query(ql, qr, i * 2 + 2, m, ir));
+      return multiply(query(ql, qr, i * 2 + 1, il, m),
+                      query(ql, qr, i * 2 + 2, m, ir));
     }
   }
 };
@@ -61,29 +49,30 @@ int main() {
   ios::sync_with_stdio(false);
   int n, q;
   cin >> n >> q;
-  vector<Monoid> a;
+  using M = int64_t;
+  SegmentTree<M> seg(n, 0, [&](M a, M b) -> M { return a + b; });
   rep(i, n) {
-    int64_t v;
-    cin >> v;
-    a.emplace_back(v);
+    int x;
+    cin >> x;
+    seg.update(i, x);
   }
-  SegmentTree<Monoid> seg(n, a);
   while (q--) {
     int t;
     cin >> t;
     if (t == 0) {
       int i, x;
       cin >> i >> x;
-      seg.update(i, Monoid(seg.get(i).val + x));
+      auto y = seg.get(i);
+      seg.update(i, y + x);
     } else {
       int l, r;
       cin >> l >> r;
-      cout << seg.query(l, r).val << endl;
+      cout << seg.query(l, r) << endl;
     }
   }
 
   return 0;
 }
 
-// https://judge.yosupo.jp/submission/1734
-// https://judge.yosupo.jp/submission/1735
+// https://judge.yosupo.jp/submission/4476
+// https://judge.yosupo.jp/submission/4478

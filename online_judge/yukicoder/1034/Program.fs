084ada8db629bda08f9be33ab2874501fd7815da
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-05-02 02:28:38 +0900

    Color Box

---
+++ online_judge/yukicoder/1034/Program.fs
@@ -0,0 +1,23 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let solve n i j =
+    let k = [i; n - i - 1L; j; n - j - 1L] |> List.min
+    k * 4L * (n - k) + 
+        if i = k then
+            j - k
+        elif j = n - k - 1L then
+            n - k * 2L - 1L + i - k
+        elif i = n - k - 1L then
+            (n - k * 2L - 1L) * 2L + n - k - 1L - j
+        else
+            (n - k * 2L - 1L) * 3L + n - k - 1L - i
+
+[<EntryPoint>]
+let main argv =
+    let q = stdin.ReadLine() |> int
+    for qq = 1 to q do
+        let [|n; i; j|] = stdin.ReadLine().Split() |> Array.map int64
+        printfn "%d" (solve n i j)
+    0 // return an integer exit code
---
+++ online_judge/yukicoder/993/Program.fs
@@ -0,0 +1,9 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main _ =
+    let s = stdin.ReadLine().Replace("ao", "ki")
+    printfn "%s" s
+    0 // return an integer exit code

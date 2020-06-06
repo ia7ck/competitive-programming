1fd197f0c9f7718591e4005375184d1ceda66a48
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-06-06 12:38:49 +0900

    A Nice XOR Pair

---
+++ online_judge/yukicoder/1072/Program.fs
@@ -0,0 +1,28 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let get (map: Map<int, int64>) (key: int) =
+    match map.TryFind key with
+    | Some f -> f
+    | None -> 0L
+
+[<EntryPoint>]
+let main argv =
+    let [| n; x |] = stdin.ReadLine().Split() |> Array.map int
+
+    let a =
+        [ for i in 1 .. n -> stdin.ReadLine() |> int ]
+
+    let res =
+        a
+        |> List.fold (fun (freq: Map<int, int64>, ans) y ->
+            let z = x ^^^ y
+            let zf = get freq z
+            let yf = get freq y
+            freq.Add(y, yf + 1L), ans + zf) (Map.empty, 0L)
+
+    res
+    |> snd
+    |> printfn "%d"
+    0 // return an integer exit code

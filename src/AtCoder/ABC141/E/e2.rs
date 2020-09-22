c87d008ea1fc83917643b98e8e280cb9800cee85
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-09-22 20:43:03 +0900

    speed up

--- src/AtCoder/ABC141/E/e2.rs
+++ src/AtCoder/ABC141/E/e2.rs
@@ -25,10 +25,12 @@ impl<R: std::io::Read> ProconReader<R> {
 }
 
 mod rolling_hash {
-    const MASK_30: u64 = (1 << 30) - 1;
-    const MASK_31: u64 = (1 << 31) - 1;
-    const M: u64 = (1 << 61) - 1;
-    const B: u64 = 1_000_000_000 + 9;
+    const MASK30: u64 = (1 << 30) - 1;
+    const MASK31: u64 = (1 << 31) - 1;
+    const MOD: u64 = (1 << 61) - 1;
+    const MASK61: u64 = (1 << 61) - 1;
+    const POSITIVIZER: u64 = MOD * 4;
+    const BASE: u64 = 1_000_000_000 + 9;
     pub struct RollingHash {
         h: Vec<u64>,
         p: Vec<u64>,
@@ -40,26 +42,35 @@ mod rolling_hash {
             let mut p = vec![0; n + 1];
             p[0] = 1;
             for i in 0..n {
-                h[i + 1] = mul(h[i], B) + s[i];
-                p[i + 1] = mul(p[i], B);
+                h[i + 1] = calc_mod(mul(h[i], BASE) + s[i]);
+                p[i + 1] = calc_mod(mul(p[i], BASE));
             }
             Self { h, p }
         }
         pub fn get(&self, range: std::ops::Range<usize>) -> u64 {
             let l = range.start;
             let r = range.end;
-            (self.h[r] + M - mul(self.h[l], self.p[r - l])) % M
+            calc_mod(self.h[r] + POSITIVIZER - mul(self.h[l], self.p[r - l]))
         }
     }
     fn mul(a: u64, b: u64) -> u64 {
         let au = a >> 31;
-        let ad = a & MASK_31;
+        let ad = a & MASK31;
         let bu = b >> 31;
-        let bd = b & MASK_31;
+        let bd = b & MASK31;
         let mid = ad * bu + au * bd;
         let midu = mid >> 30;
-        let midd = mid & MASK_30;
-        (au * bu * 2 + midu + (midd << 31) + ad * bd) % M
+        let midd = mid & MASK30;
+        au * bu * 2 + midu + (midd << 31) + ad * bd
+    }
+    fn calc_mod(x: u64) -> u64 {
+        let xu = x >> 61;
+        let xd = x & MASK61;
+        let mut res = xu + xd;
+        if res >= MOD {
+            res -= MOD;
+        }
+        res
     }
 }
 

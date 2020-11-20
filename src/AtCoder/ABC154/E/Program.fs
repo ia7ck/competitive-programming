6a578e7098ffe4f3742c73ce6ec279a152c97605
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-11-21 02:00:04 +0900

    dp

---
+++ src/AtCoder/ABC154/E/Program.fs
@@ -0,0 +1,43 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Collections.Generic
+
+let memorec f =
+    let memo = Dictionary<_, _>()
+
+    let rec frec a =
+        match memo.TryGetValue a with
+        | exist, value when exist -> value
+        | _ ->
+            let value = f frec a
+            memo.Add(a, value)
+            value
+
+    frec
+
+open System.Numerics
+
+let f =
+    fun solve n k ->
+        if k = 0 then
+            // 残り上の桁全て 0 で埋める
+            1I
+        elif n < 10I then
+            if k = 1 then n else 0I
+        else
+            let q = n / 10I
+            let r = n % 10I
+
+            (solve q k)
+            + r * (solve q (k - 1))
+            + (9I - r) * (solve (q - 1I) (k - 1))
+
+[<EntryPoint>]
+let main _ =
+    let n = stdin.ReadLine() |> BigInteger.Parse
+    let k = stdin.ReadLine() |> int
+
+    printfn "%A" ((memorec f) n k)
+
+    0 // return an integer exit code
---
+++ src/AtCoder/dp/B/Program.fs
@@ -0,0 +1,39 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open System.Collections.Generic
+
+let memorec f =
+    let memo = Dictionary<_, _>()
+
+    let rec frec a =
+        match memo.TryGetValue a with
+        | exist, value when exist -> value
+        | _ ->
+            let value = f frec a
+            memo.Add(a, value)
+            value
+
+    frec
+
+[<EntryPoint>]
+let main _ =
+
+    let [| n; k |] =
+        stdin.ReadLine().Split() |> Array.map int
+
+    let h =
+        stdin.ReadLine().Split() |> Array.map int
+
+    let f =
+        fun solve i ->
+            if i = 0 then
+                0
+            else
+                seq { i - k .. i - 1 }
+                |> Seq.filter (fun j -> j >= 0)
+                |> Seq.map (fun j -> abs (h.[j] - h.[i]) + (solve j))
+                |> Seq.min
+
+    printfn "%d" ((memorec f) (n - 1))
+    0 // return an integer exit code

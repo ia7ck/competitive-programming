c8250af4e2831bbc77074608102ef822d085ddf0
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-11-20 22:48:38 +0900

    yukicoder 275

---
+++ src/yukicoder/1291/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main _ =
+    let n = stdin.ReadLine() |> int
+
+    for _ in 1 .. n do
+        let d = stdin.ReadLine() |> int
+
+        if d = 1 then
+            "10"
+        else
+            let suf =
+                String.concat "" (Seq.init (d - 1) (fun _ -> "0"))
+
+            "9" + suf
+        |> printfn "%s"
+
+    0 // return an integer exit code

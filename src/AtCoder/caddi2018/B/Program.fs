eddbd44c0d59924c0267d4c0a821f8b09d1b5e4f
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-11-29 11:39:21 +0900

    game

---
+++ src/AtCoder/caddi2018/B/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let n = stdin.ReadLine() |> int
+    let a = [ 1 .. n ] |> List.map (fun _ -> stdin.ReadLine() |> int)
+
+    let ans =
+        if a |> List.forall (fun x -> x % 2 = 0) then "second" else "first"
+
+    printfn "%s" ans
+    0 // return an integer exit code

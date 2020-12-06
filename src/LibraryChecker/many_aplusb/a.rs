a0f6e2e99755b05103d23fc185a74ba6db68b276
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-12-06 18:03:11 +0900

    update reader

--- src/LibraryChecker/many_aplusb/a.rs
+++ src/LibraryChecker/many_aplusb/a.rs
@@ -1,36 +1,61 @@
-pub struct ProconReader<R: std::io::Read> {
-    reader: R,
-}
-
-impl<R: std::io::Read> ProconReader<R> {
-    pub fn new(reader: R) -> Self {
-        Self { reader }
-    }
-    pub fn get<T: std::str::FromStr>(&mut self) -> T {
-        use std::io::Read;
-        let buf = self
-            .reader
-            .by_ref()
-            .bytes()
-            .map(|b| b.unwrap())
-            .skip_while(|&byte| byte == b' ' || byte == b'\n' || byte == b'\r')
-            .take_while(|&byte| byte != b' ' && byte != b'\n' && byte != b'\r')
-            .collect::<Vec<_>>();
-        std::str::from_utf8(&buf)
-            .unwrap()
-            .parse()
-            .ok()
-            .expect("Parse Error.")
-    }
-}
-
 fn main() {
     let stdin = std::io::stdin();
-    let mut rd = ProconReader::new(stdin.lock());
+    let mut rd = ProconReader::new(std::io::BufReader::new(stdin));
+
     let t: usize = rd.get();
+    let mut ans = vec![];
     for _ in 0..t {
-        let a: i64 = rd.get();
-        let b: i64 = rd.get();
-        println!("{}", a + b);
+        let a: u64 = rd.get();
+        let b: u64 = rd.get();
+        ans.push((a + b).to_string());
+    }
+    println!("{}", ans.join("\n"));
+}
+
+pub struct ProconReader<R> {
+    r: R,
+    line: String,
+    i: usize,
+}
+
+impl<R: std::io::BufRead> ProconReader<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            r: reader,
+            line: String::new(),
+            i: 0,
+        }
+    }
+    pub fn get<T>(&mut self) -> T
+    where
+        T: std::str::FromStr,
+        <T as std::str::FromStr>::Err: std::fmt::Debug,
+    {
+        self.skip_blanks();
+        assert!(self.i < self.line.len());
+        assert_ne!(&self.line[self.i..=self.i], " ");
+        let line = &self.line[self.i..];
+        let end = line.find(' ').unwrap_or(line.len());
+        let s = &line[..end];
+        self.i += end;
+        s.parse().expect(&format!("parse error `{}`", self.line))
+    }
+    fn skip_blanks(&mut self) {
+        loop {
+            let start = self.line[self.i..].find(|ch| ch != ' ');
+            match start {
+                Some(j) => {
+                    self.i += j;
+                    break;
+                }
+                None => {
+                    self.line.clear();
+                    self.i = 0;
+                    let num_bytes = self.r.read_line(&mut self.line).expect("not valid utf-8");
+                    assert!(num_bytes > 0, "reached EOF :(");
+                    self.line = self.line.trim_end_matches(&['\r', '\n'][..]).to_string();
+                }
+            }
+        }
     }
 }

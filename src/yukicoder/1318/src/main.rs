f742ac109cee5e4bbd1b9c9420317f488a52855e
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-12-16 20:14:50 +0900

    yuki 1318

---
+++ src/yukicoder/1318/src/main.rs
@@ -0,0 +1,99 @@
+fn main() {
+    let stdin = std::io::stdin();
+    let mut rd = ProconReader::new(stdin.lock());
+
+    let n: usize = rd.get();
+    let m: usize = rd.get();
+    let mut ans = vec![0u64; n + 1];
+    for a in 0..=m {
+        for b in a..=m {
+            for c in b..=m {
+                for d in c..=m {
+                    let s = a * a
+                        + a * b
+                        + a * c
+                        + a * d
+                        + b * b
+                        + b * c
+                        + b * d
+                        + c * c
+                        + c * d
+                        + d * d;
+                    if s > n {
+                        break;
+                    }
+                    if a == b && b == c && c == d && d == a {
+                        ans[s] += 1;
+                    } else if (a == b && b == c) || (b == c && c == d) || (c == d && d == a) {
+                        ans[s] += 4;
+                    } else if (a == b && c == d) || (b == c && d == a) {
+                        ans[s] += 6;
+                    } else if (a == b) || (b == c) || (c == d) || (d == a) {
+                        ans[s] += 12;
+                    } else {
+                        ans[s] += 24;
+                    }
+                }
+            }
+        }
+    }
+    for a in ans {
+        println!("{}", a);
+    }
+}
+
+pub struct ProconReader<R> {
+    r: R,
+    line: String,
+    i: usize,
+}
+
+impl<R: std::io::BufRead> ProconReader<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            r: reader,
+            line: String::new(),
+            i: 0,
+        }
+    }
+    pub fn get<T>(&mut self) -> T
+    where
+        T: std::str::FromStr,
+        <T as std::str::FromStr>::Err: std::fmt::Debug,
+    {
+        self.skip_blanks();
+        assert!(self.i < self.line.len());
+        assert_ne!(&self.line[self.i..=self.i], " ");
+        let line = &self.line[self.i..];
+        let end = line.find(' ').unwrap_or_else(|| line.len());
+        let s = &line[..end];
+        self.i += end;
+        s.parse()
+            .unwrap_or_else(|_| panic!("parse error `{}`", self.line))
+    }
+    fn skip_blanks(&mut self) {
+        loop {
+            let start = self.line[self.i..].find(|ch| ch != ' ');
+            match start {
+                Some(j) => {
+                    self.i += j;
+                    break;
+                }
+                None => {
+                    self.line.clear();
+                    self.i = 0;
+                    let num_bytes = self.r.read_line(&mut self.line).unwrap();
+                    assert!(num_bytes > 0, "reached EOF :(");
+                    self.line = self.line.trim_end_matches(&['\r', '\n'][..]).to_string();
+                }
+            }
+        }
+    }
+    pub fn get_vec<T>(&mut self, n: usize) -> Vec<T>
+    where
+        T: std::str::FromStr,
+        <T as std::str::FromStr>::Err: std::fmt::Debug,
+    {
+        (0..n).map(|_| self.get()).collect()
+    }
+}

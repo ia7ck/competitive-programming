5324cba8ca50b03b4ed391dddb35eae8f98183f3
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-12-23 21:50:30 +0900

    cmb

--- src/AtCoder/ABC154/F/f.rs
+++ src/AtCoder/ABC154/F/f.rs
@@ -1,243 +1,125 @@
-use std::io::Read;
-
-fn read<T: std::str::FromStr>() -> T {
-    let token: String = std::io::stdin()
-        .bytes()
-        .map(|c| c.ok().unwrap() as char)
-        .skip_while(|c| c.is_whitespace())
-        .take_while(|c| !c.is_whitespace())
-        .collect();
-    token.parse().ok().unwrap()
-}
-
-#[allow(dead_code)]
-mod mint {
-    use std::ops::{Add, BitAnd, Div, Mul, Rem, Shr, Sub};
-
-    #[derive(Copy, Clone)]
-    pub struct Mint<T> {
-        x: T,
-        mo: T,
-    }
-    impl<T> Mint<T>
-    where
-        T: Copy,
-    {
-        pub fn new(x: T, mo: T) -> Mint<T> {
-            Mint { x, mo }
-        }
-    }
-    impl<T> Mint<T>
-    where
-        T: Copy,
-    {
-        pub fn val(&self) -> T {
-            self.x
-        }
-        pub fn mo(&self) -> T {
-            self.mo
-        }
-    }
-    impl<T> Add<T> for Mint<T>
-    where
-        T: Copy,
-        T: Add<Output = T>,
-        T: Rem<Output = T>,
-    {
-        type Output = Mint<T>;
-        fn add(self, rhs: T) -> Mint<T> {
-            Mint::new((self.val() + rhs % self.mo()) % self.mo(), self.mo())
-        }
-    }
-    impl<T> Add<Mint<T>> for Mint<T>
-    where
-        T: Copy,
-        Mint<T>: Add<T, Output = Mint<T>>,
-    {
-        type Output = Mint<T>;
-        fn add(self, rhs: Mint<T>) -> Mint<T> {
-            self + rhs.val()
-        }
-    }
-    impl<T> Sub<T> for Mint<T>
-    where
-        T: Copy,
-        T: Add<Output = T>,
-        T: Sub<Output = T>,
-        T: Rem<Output = T>,
-    {
-        type Output = Mint<T>;
-        fn sub(self, rhs: T) -> Mint<T> {
-            Mint::new(
-                (self.val() + self.mo() - rhs % self.mo()) % self.mo(),
-                self.mo(),
-            )
-        }
-    }
-    impl<T> Sub<Mint<T>> for Mint<T>
-    where
-        T: Copy,
-        Mint<T>: Sub<T, Output = Mint<T>>,
-    {
-        type Output = Mint<T>;
-        fn sub(self, rhs: Mint<T>) -> Mint<T> {
-            self - rhs.val()
-        }
-    }
-    impl<T> Mul<T> for Mint<T>
-    where
-        T: Copy,
-        T: Mul<Output = T>,
-        T: Rem<Output = T>,
-    {
-        type Output = Mint<T>;
-        fn mul(self, rhs: T) -> Mint<T> {
-            Mint::new((self.val() * rhs % self.mo()) % self.mo(), self.mo())
-        }
-    }
-    impl<T> Mul<Mint<T>> for Mint<T>
-    where
-        T: Copy,
-        Mint<T>: Mul<T, Output = Mint<T>>,
-    {
-        type Output = Mint<T>;
-        fn mul(self, rhs: Mint<T>) -> Mint<T> {
-            self * rhs.val()
-        }
-    }
-
-    impl<T> Mint<T>
-    where
-        T: Copy,
-        T: Sub<Output = T>,
-        T: Div<Output = T>,
-        T: PartialOrd,
-        T: PartialEq,
-        T: BitAnd<Output = T>,
-        T: Shr<Output = T>,
-        Mint<T>: Mul<Output = Mint<T>>,
-    {
-        pub fn pow(self, y: T) -> Mint<T> {
-            let one = self.mo() / self.mo();
-            let zero = self.mo() - self.mo();
-            let mut res = Mint::one(self.mo());
-            let mut base = self;
-            let mut exp = y;
-            while exp > zero {
-                if (exp & one) == one {
-                    res = res * base;
-                }
-                base = base * base;
-                exp = exp >> one;
-            }
-            res
-        }
-    }
-    impl<T> Div<T> for Mint<T>
-    where
-        T: Copy,
-        T: Sub<Output = T>,
-        T: Div<Output = T>,
-        T: PartialOrd,
-        T: PartialEq,
-        T: BitAnd<Output = T>,
-        T: Shr<Output = T>,
-        Mint<T>: Mul<Output = Mint<T>>,
-    {
-        type Output = Mint<T>;
-        fn div(self, rhs: T) -> Mint<T> {
-            let one = self.mo() / self.mo();
-            self * Mint::new(rhs, self.mo()).pow(self.mo() - one - one)
-        }
-    }
-    impl<T> Div<Mint<T>> for Mint<T>
-    where
-        T: Copy,
-        Mint<T>: Div<T, Output = Mint<T>>,
-    {
-        type Output = Mint<T>;
-        fn div(self, rhs: Mint<T>) -> Mint<T> {
-            self / rhs.val()
-        }
-    }
-    impl<T> Mint<T>
-    where
-        T: Copy,
-        T: Div<Output = T>,
-        Mint<T>: Div<Output = Mint<T>>,
-    {
-        pub fn inv(self) -> Mint<T> {
-            Mint::one(self.mo()) / self
-        }
-    }
-    impl<T> std::fmt::Display for Mint<T>
-    where
-        T: Copy + std::fmt::Display,
-    {
-        fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-            write!(f, "{}", self.val())
-        }
-    }
-    impl<T> Mint<T>
-    where
-        T: Copy,
-        T: Sub<Output = T>,
-    {
-        pub fn zero(mo: T) -> Mint<T> {
-            Mint { x: mo - mo, mo }
-        }
-    }
-    impl<T> Mint<T>
-    where
-        T: Copy,
-        T: Div<Output = T>,
-    {
-        pub fn one(mo: T) -> Mint<T> {
-            Mint { x: mo / mo, mo }
-        }
-    }
-}
-
-use mint::Mint;
-
 fn main() {
-    let r1: usize = read();
-    let c1: usize = read();
-    let r2: usize = read();
-    let c2: usize = read();
+    let stdin = std::io::stdin();
+    let mut rd = ProconReader::new(stdin.lock());
 
-    let mo = 1000000007;
-    let solve = |r: usize, c: usize| -> Mint<usize> {
-        let (fac, fac_inv) = (|n, mo| {
-            let mut fac = vec![Mint::new(0, mo); n];
-            let mut fac_inv = vec![Mint::new(0, mo); n];
-            fac[0] = Mint::new(1, mo);
-            for i in 1..n {
-                fac[i] = fac[i - 1] * i;
-            }
-            fac_inv[n - 1] = fac[n - 1].inv();
-            for i in (0..n - 1).rev() {
-                fac_inv[i] = fac_inv[i + 1] * (i + 1);
-            }
-            (fac, fac_inv)
-        })(r + c + 2, mo);
-        let binom = |a: usize, b: usize| {
-            if a < b {
-                return Mint::new(0, mo);
-            }
-            fac[a] * fac_inv[b] * fac_inv[a - b]
-        };
-        let mut ans = Mint::new(0, mo);
-        for i in 0..=r {
-            ans = ans + binom((i + 1) + c, c);
-        }
-        ans
+    let r1: usize = rd.get();
+    let c1: usize = rd.get();
+    let r2: usize = rd.get();
+    let c2: usize = rd.get();
+
+    let mo = 1_000_000_000 + 7;
+    let binom = Binom::new(r2 + c2 + 2, mo);
+    let g = |r: usize, c: usize| {
+        (0..=r)
+            .map(|i| binom.get(i + c + 1, c))
+            .fold(0, |acc, x| (acc + x) % mo)
     };
+    let ans = g(r2, c2) - g(r1 - 1, c2) - g(r2, c1 - 1) + g(r1 - 1, c1 - 1);
+    println!("{}", ans.rem_euclid(mo));
+}
 
-    let mut ans = Mint::new(0, mo);
-    ans = ans + solve(r2, c2);
-    ans = ans - solve(r1 - 1, c2);
-    ans = ans - solve(r2, c1 - 1);
-    ans = ans + solve(r1 - 1, c1 - 1);
-    println!("{}", ans);
+use std::fmt::Debug;
+use std::str::FromStr;
+
+pub struct ProconReader<R> {
+    r: R,
+    line: String,
+    i: usize,
+}
+
+impl<R: std::io::BufRead> ProconReader<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            r: reader,
+            line: String::new(),
+            i: 0,
+        }
+    }
+    pub fn get<T>(&mut self) -> T
+    where
+        T: FromStr,
+        <T as FromStr>::Err: Debug,
+    {
+        self.skip_blanks();
+        assert!(self.i < self.line.len());
+        assert_ne!(&self.line[self.i..=self.i], " ");
+        let line = &self.line[self.i..];
+        let end = line.find(' ').unwrap_or_else(|| line.len());
+        let s = &line[..end];
+        self.i += end;
+        s.parse()
+            .unwrap_or_else(|_| panic!("parse error `{}`", self.line))
+    }
+    fn skip_blanks(&mut self) {
+        loop {
+            let start = self.line[self.i..].find(|ch| ch != ' ');
+            match start {
+                Some(j) => {
+                    self.i += j;
+                    break;
+                }
+                None => {
+                    self.line.clear();
+                    self.i = 0;
+                    let num_bytes = self.r.read_line(&mut self.line).unwrap();
+                    assert!(num_bytes > 0, "reached EOF :(");
+                    self.line = self.line.trim_end_matches(&['\r', '\n'][..]).to_string();
+                }
+            }
+        }
+    }
+    pub fn get_vec<T>(&mut self, n: usize) -> Vec<T>
+    where
+        T: FromStr,
+        <T as FromStr>::Err: Debug,
+    {
+        (0..n).map(|_| self.get()).collect()
+    }
+}
+
+pub trait BinomialCoefficient {
+    type Output;
+    fn get(&self, n: usize, k: usize) -> Self::Output;
+}
+
+pub struct Binom {
+    size: usize,
+    mo: i64,
+    fac: Vec<i64>,
+    inv_fac: Vec<i64>,
+}
+
+impl Binom {
+    pub fn new(size: usize, mo: i64) -> Self {
+        let mut fac = vec![0; size];
+        let mut inv = vec![0; size];
+        let mut inv_fac = vec![0; size];
+        fac[0] = 1;
+        fac[1] = 1;
+        inv[1] = 1;
+        inv_fac[0] = 1;
+        inv_fac[1] = 1;
+        for i in 2..size {
+            fac[i] = fac[i - 1] * (i as i64) % mo;
+            inv[i] = (-inv[(mo as usize) % i] * (mo / (i as i64))).rem_euclid(mo);
+            inv_fac[i] = inv_fac[i - 1] * inv[i] % mo;
+        }
+        Self {
+            size,
+            mo,
+            fac,
+            inv_fac,
+        }
+    }
+}
+
+impl BinomialCoefficient for Binom {
+    type Output = i64;
+    fn get(&self, n: usize, k: usize) -> Self::Output {
+        assert!(n < self.size);
+        if n < k {
+            return 0;
+        }
+        ((self.fac[n] * self.inv_fac[k]) % self.mo * self.inv_fac[n - k]) % self.mo
+    }
 }

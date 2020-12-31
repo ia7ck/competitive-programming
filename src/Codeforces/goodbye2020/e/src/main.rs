af887d6cd127666a6c359576d46f289e59ecbfaf
ia7ck <23146842+ia7ck@users.noreply.github.com>
2020-12-31 11:17:05 +0900

    upsolve goodbye2020 E

---
+++ src/Codeforces/goodbye2020/e/src/main.rs
@@ -0,0 +1,214 @@
+fn solve(n: usize, x: Vec<i64>) {
+    const L: usize = 60;
+    type Mint = ModInt1000000007;
+    let mut popcnt_sum = [Mint::new(0); L];
+    for p in 0..L {
+        for &x in &x {
+            popcnt_sum[p] = popcnt_sum[p] + Mint::new(x >> p & 1);
+        }
+    }
+    let mut ans = Mint::new(0);
+    for j in 0..n {
+        let mut and_sum = Mint::new(0);
+        let mut or_sum = Mint::new(0);
+        let mut pow2 = Mint::new(1);
+        for p in 0..L {
+            and_sum = and_sum + pow2 * Mint::new(x[j] >> p & 1) * popcnt_sum[p];
+            let nn = Mint::new(n as i64);
+            or_sum = or_sum + pow2 * (nn - Mint::new((x[j] >> p & 1) ^ 1) * (nn - popcnt_sum[p]));
+            pow2 = pow2 * Mint::new(2);
+        }
+        ans = ans + and_sum * or_sum;
+    }
+    println!("{}", ans.val());
+}
+
+fn main() {
+    let stdin = std::io::stdin();
+    let mut rd = ProconReader::new(stdin.lock());
+
+    let t: usize = rd.get();
+    for _ in 0..t {
+        let n: usize = rd.get();
+        let x: Vec<i64> = rd.get_vec(n);
+        solve(n, x);
+    }
+}
+
+use std::fmt::Debug;
+use std::marker::PhantomData;
+use std::ops::{Add, Div, Mul, Sub};
+
+pub trait Modulo: Copy + Clone + Debug {
+    fn p() -> i64;
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct ModInt<M>(i64, PhantomData<M>);
+
+impl<M: Modulo> ModInt<M> {
+    pub fn new<T>(x: T) -> Self
+    where
+        T: Into<i64>,
+    {
+        let x = x.into();
+        if 0 <= x && x < M::p() {
+            Self::new_raw(x)
+        } else {
+            Self::new_raw(x.rem_euclid(M::p()))
+        }
+    }
+
+    pub fn new_raw(x: i64) -> Self {
+        Self(x, PhantomData)
+    }
+
+    pub fn val(self) -> i64 {
+        self.0
+    }
+
+    pub fn mo() -> i64 {
+        M::p()
+    }
+
+    pub fn pow(self, exp: u64) -> Self {
+        let mut res = 1;
+        let mut base = self.0;
+        let mut exp = exp;
+        let mo = Self::mo();
+        while exp > 0 {
+            if exp & 1 == 1 {
+                res *= base;
+                res %= mo;
+            }
+            base *= base;
+            base %= mo;
+            exp >>= 1;
+        }
+        Self::new_raw(res)
+    }
+
+    pub fn inv(self) -> Self {
+        assert_ne!(self.0, 0, "Don't divide by zero!");
+        self.pow(Self::mo() as u64 - 2)
+    }
+
+    pub fn new_frac(numer: i64, denom: i64) -> Self {
+        Self::new(numer) / Self::new(denom)
+    }
+}
+
+#[allow(clippy::suspicious_arithmetic_impl)]
+impl<M: Modulo> Add for ModInt<M> {
+    type Output = ModInt<M>;
+    fn add(self, rhs: ModInt<M>) -> Self::Output {
+        let x = self.0 + rhs.0;
+        debug_assert!(x >= 0);
+        if x < Self::mo() {
+            Self::new_raw(x)
+        } else {
+            Self::new_raw(x - Self::mo())
+        }
+    }
+}
+
+#[allow(clippy::suspicious_arithmetic_impl)]
+impl<M: Modulo> Sub for ModInt<M> {
+    type Output = ModInt<M>;
+    fn sub(self, rhs: ModInt<M>) -> Self::Output {
+        let x = self.0 - rhs.0;
+        debug_assert!(x < Self::mo());
+        if x >= 0 {
+            Self::new_raw(x)
+        } else {
+            Self::new_raw(x + Self::mo())
+        }
+    }
+}
+
+impl<M: Modulo> Mul for ModInt<M> {
+    type Output = ModInt<M>;
+    fn mul(self, rhs: ModInt<M>) -> Self::Output {
+        Self::new(self.0 * rhs.0)
+    }
+}
+
+#[allow(clippy::suspicious_arithmetic_impl)]
+impl<M: Modulo> Div for ModInt<M> {
+    type Output = ModInt<M>;
+    fn div(self, rhs: ModInt<M>) -> Self::Output {
+        self * rhs.inv()
+    }
+}
+
+#[macro_export]
+macro_rules! define_mod_int_p {
+    ($mod: ident, $mod_int: ident, $p: expr) => {
+        #[derive(Clone, Copy, Debug)]
+        pub struct $mod;
+        impl Modulo for $mod {
+            fn p() -> i64 {
+                $p
+            }
+        }
+        pub type $mod_int = ModInt<$mod>;
+    };
+}
+define_mod_int_p!(Mod1000000007, ModInt1000000007, 1_000_000_000 + 7);
+define_mod_int_p!(Mod998244353, ModInt998244353, 998_244_353);
+
+pub struct ProconReader<R> {
+    r: R,
+    line: String,
+    i: usize,
+}
+
+impl<R: std::io::BufRead> ProconReader<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            r: reader,
+            line: String::new(),
+            i: 0,
+        }
+    }
+    pub fn get<T>(&mut self) -> T
+    where
+        T: std::str::FromStr,
+        <T as std::str::FromStr>::Err: std::fmt::Debug,
+    {
+        self.skip_blanks();
+        assert!(self.i < self.line.len());
+        assert_ne!(&self.line[self.i..=self.i], " ");
+        let line = &self.line[self.i..];
+        let end = line.find(' ').unwrap_or_else(|| line.len());
+        let s = &line[..end];
+        self.i += end;
+        s.parse()
+            .unwrap_or_else(|_| panic!("parse error `{}`", self.line))
+    }
+    fn skip_blanks(&mut self) {
+        loop {
+            let start = self.line[self.i..].find(|ch| ch != ' ');
+            match start {
+                Some(j) => {
+                    self.i += j;
+                    break;
+                }
+                None => {
+                    self.line.clear();
+                    self.i = 0;
+                    let num_bytes = self.r.read_line(&mut self.line).unwrap();
+                    assert!(num_bytes > 0, "reached EOF :(");
+                    self.line = self.line.trim_end_matches(&['\r', '\n'][..]).to_string();
+                }
+            }
+        }
+    }
+    pub fn get_vec<T>(&mut self, n: usize) -> Vec<T>
+    where
+        T: std::str::FromStr,
+        <T as std::str::FromStr>::Err: std::fmt::Debug,
+    {
+        (0..n).map(|_| self.get()).collect()
+    }
+}

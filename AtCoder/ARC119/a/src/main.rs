0715260dc93af5bab50efdad6d02a2825f163510
ia7ck <23146842+ia7ck@users.noreply.github.com>
2021-07-03 11:10:46 +0900

    ARC 119

---
+++ AtCoder/ARC119/a/src/main.rs
@@ -0,0 +1,83 @@
+fn main() {
+    let stdin = std::io::stdin();
+    let mut rd = ProconReader::new(stdin.lock());
+
+    let n: u128 = rd.get();
+
+    let mut ans = n;
+    for b in 0..=60 {
+        let p = 1u128 << b;
+        let a = n / p;
+        let c = n % p;
+        if n == a * p + c {
+            ans = ans.min(a + b + c);
+        }
+    }
+    println!("{}", ans);
+}
+
+pub struct ProconReader<R> {
+    r: R,
+    l: String,
+    i: usize,
+}
+
+impl<R: std::io::BufRead> ProconReader<R> {
+    pub fn new(reader: R) -> Self {
+        Self {
+            r: reader,
+            l: String::new(),
+            i: 0,
+        }
+    }
+    pub fn get<T>(&mut self) -> T
+        where
+            T: std::str::FromStr,
+            <T as std::str::FromStr>::Err: std::fmt::Debug,
+    {
+        self.skip_blanks();
+        assert!(self.i < self.l.len()); // remain some character
+        assert_ne!(&self.l[self.i..=self.i], " ");
+        let rest = &self.l[self.i..];
+        let len = rest.find(' ').unwrap_or_else(|| rest.len());
+        // parse self.l[self.i..(self.i + len)]
+        let val = rest[..len]
+            .parse()
+            .unwrap_or_else(|e| panic!("{:?}, attempt to read `{}`", e, rest));
+        self.i += len;
+        val
+    }
+    fn skip_blanks(&mut self) {
+        loop {
+            match self.l[self.i..].find(|ch| ch != ' ') {
+                Some(j) => {
+                    self.i += j;
+                    break;
+                }
+                None => {
+                    let mut buf = String::new();
+                    let num_bytes = self
+                        .r
+                        .read_line(&mut buf)
+                        .unwrap_or_else(|_| panic!("invalid UTF-8"));
+                    assert!(num_bytes > 0, "reached EOF :(");
+                    self.l = buf
+                        .trim_end_matches('\n')
+                        .trim_end_matches('\r')
+                        .to_string();
+                    self.i = 0;
+                }
+            }
+        }
+    }
+    pub fn get_vec<T>(&mut self, n: usize) -> Vec<T>
+        where
+            T: std::str::FromStr,
+            <T as std::str::FromStr>::Err: std::fmt::Debug,
+    {
+        (0..n).map(|_| self.get()).collect()
+    }
+    pub fn get_chars(&mut self) -> Vec<char> {
+        self.get::<String>().chars().collect()
+    }
+}

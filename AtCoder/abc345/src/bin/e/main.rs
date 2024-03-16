0014920ed5ab5945b8cc507d5e6230b9334af534
ia7ck <23146842+ia7ck@users.noreply.github.com>
2024-03-17 02:19:03 +0900

    upsolve abc345e

--- AtCoder/abc345/src/bin/e/main.rs
+++ AtCoder/abc345/src/bin/e/main.rs
@@ -1,9 +1,6 @@
-use std::cmp::Reverse;
+use std::{cmp::Reverse, mem};
 
 use proconio::{input, marker::Usize1};
-
-// TODO: speed up
-// https://atcoder.jp/contests/abc345/submissions/51355681
 
 fn main() {
     input! {
@@ -16,8 +13,12 @@ fn main() {
     let mut dp = vec![vec![]; k + 1];
     dp[0] = vec![balls[0]];
     dp[1] = vec![(usize::MAX, 0)];
+    // avoid to allocate memory inside for-loop
+    let mut new_dp = vec![vec![]; k + 1];
     for &(c, v) in &balls[1..] {
-        let mut new_dp = vec![vec![]; k + 1];
+        for j in 0..=k {
+            new_dp[j].clear();
+        }
         for j in 0..=k {
             assert!(dp[j].len() <= 2);
 
@@ -41,7 +42,7 @@ fn main() {
             new_dp[j].dedup_by_key(|&mut (last, _)| last);
             new_dp[j].truncate(2);
         }
-        dp = new_dp;
+        mem::swap(&mut dp, &mut new_dp);
     }
 
     if let Some((_, ans)) = dp[k].get(0) {

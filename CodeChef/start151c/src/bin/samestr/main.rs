15d03275a187b0b8a037c37bf628308073fe0ec2
ia7ck <23146842+ia7ck@users.noreply.github.com>
2024-09-21 21:00:05 +0900

    start 151

---
+++ CodeChef/start151c/src/bin/samestr/main.rs
@@ -0,0 +1,137 @@
+use scanner::Scanner;
+use std::io;
+
+fn main() {
+    let mut scanner = Scanner::from(io::stdin().lock());
+
+    let t = scan!(usize, <~scanner);
+    for _ in 0..t {
+        let n = scan!(usize, <~scanner);
+        let s = scan!(String, <~scanner);
+        solve(n, s);
+    }
+}
+
+fn solve(n: usize, s: String) {
+    let mut zero = 0;
+    let mut one = 0;
+    for c in s.chars() {
+        if c == '0' {
+            zero += 1;
+        } else {
+            one += 1;
+        }
+    }
+
+    let mut ans = 0;
+    for k in 1..=n {
+        if [zero, one]
+            .iter()
+            .any(|&x| k >= x && ((k - x) % 2 == 0 || (k - x) % n == 0))
+        {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
+}
+
+// ✂ --- scanner --- ✂
+#[allow(unused)]
+mod scanner {
+    use std::fmt;
+    use std::io;
+    use std::str;
+
+    pub struct Scanner<R> {
+        r: R,
+        l: String,
+        i: usize,
+    }
+
+    impl<R> Scanner<R>
+    where
+        R: io::BufRead,
+    {
+        pub fn new(reader: R) -> Self {
+            Self {
+                r: reader,
+                l: String::new(),
+                i: 0,
+            }
+        }
+
+        pub fn scan<T>(&mut self) -> T
+        where
+            T: str::FromStr,
+            T::Err: fmt::Debug,
+        {
+            self.skip_blanks();
+            assert!(self.i < self.l.len()); // remain some character
+            assert_ne!(&self.l[self.i..=self.i], " ");
+            let rest = &self.l[self.i..];
+            let len = rest
+                .find(|ch| char::is_ascii_whitespace(&ch))
+                .unwrap_or_else(|| rest.len());
+            // parse self.l[self.i..(self.i + len)]
+            let val = rest[..len]
+                .parse()
+                .unwrap_or_else(|e| panic!("{:?}, attempt to read `{}`", e, rest));
+            self.i += len;
+            val
+        }
+
+        pub fn scan_vec<T>(&mut self, n: usize) -> Vec<T>
+        where
+            T: str::FromStr,
+            T::Err: fmt::Debug,
+        {
+            (0..n).map(|_| self.scan()).collect::<Vec<_>>()
+        }
+
+        fn skip_blanks(&mut self) {
+            loop {
+                match self.l[self.i..].find(|ch| !char::is_ascii_whitespace(&ch)) {
+                    Some(j) => {
+                        self.i += j;
+                        break;
+                    }
+                    None => {
+                        self.l.clear(); // clear buffer
+                        let num_bytes = self
+                            .r
+                            .read_line(&mut self.l)
+                            .unwrap_or_else(|_| panic!("invalid UTF-8"));
+                        assert!(num_bytes > 0, "reached EOF :(");
+                        self.i = 0;
+                    }
+                }
+            }
+        }
+    }
+
+    impl<'a> From<&'a str> for Scanner<&'a [u8]> {
+        fn from(s: &'a str) -> Self {
+            Self::new(s.as_bytes())
+        }
+    }
+
+    impl<'a> From<io::StdinLock<'a>> for Scanner<io::BufReader<io::StdinLock<'a>>> {
+        fn from(stdin: io::StdinLock<'a>) -> Self {
+            Self::new(io::BufReader::new(stdin))
+        }
+    }
+
+    #[macro_export]
+    macro_rules! scan {
+        (( $($t: ty),+ ), <~ $scanner: expr) => {
+            ( $(scan!($t, <~ $scanner)),+ )
+        };
+        ([ $t: tt; $n: expr ], <~ $scanner: expr) => {
+            (0..$n).map(|_| scan!($t, <~ $scanner)).collect::<Vec<_>>()
+        };
+        ($t: ty, <~ $scanner: expr) => {
+            $scanner.scan::<$t>()
+        };
+    }
+}
+// ✂ --- scanner --- ✂

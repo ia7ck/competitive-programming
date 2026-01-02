7882e592c5dffc04b22761ceb06f35f3b13a33b4
ia7ck <23146842+ia7ck@users.noreply.github.com>
2026-01-02 18:40:42 +0900

    speed up

--- practice/virtual-contest-2025/src/main/scala/ABC424C/Main.scala
+++ practice/virtual-contest-2025/src/main/scala/ABC424C/Main.scala
@@ -1,33 +1,33 @@
 package ABC424C
 
 import scala.collection.mutable.Queue
+import scala.collection.mutable.ArrayBuffer
+import java.io.BufferedReader
+import java.util.StringTokenizer
+import java.io.InputStreamReader
 
 @main def main() =
-  val n = scala.io.StdIn.readInt()
+  val scanner = FastScanner(java.lang.System.in)
+  val n = scanner.nextInt()
   val ab = List.fill(n) {
-    val Array(a, b) = scala.io.StdIn.readLine().split(' ').map(_.toInt)
+    val a = scanner.nextInt()
+    val b = scanner.nextInt()
     (a, b)
   }
 
-  val edges = for {
-    ((a, b), i) <- ab.zipWithIndex
-    if (a, b) != (0, 0)
-    e <- List((a, i + 1), (b, i + 1))
-  } yield e
+  val (starts, edges) = ab.zipWithIndex.partitionMap {
+    case ((0, 0), i) => Left(i + 1)
+    case ((a, b), i) => Right(List((a, i + 1), (b, i + 1)))
+  }
 
-  val graph = edges
-    .groupMap((from, _) => from)((_, to) => to)
-    // toArray
-    .foldLeft(Array.fill(n + 1) { List.empty[Int] }) {
-      case (graph, (from, to)) =>
-        graph(from) = to
-        graph
-    }
+  val graph = Array.fill(n + 1) { ArrayBuffer.empty[Int] }
+  for {
+    es <- edges
+    (from, to) <- es
+  } {
+    graph(from) += to
+  }
 
-  val starts = ab.zipWithIndex
-    .filter { case ((a, b), _) => (a, b) == (0, 0) }
-    .map((_, i) => i + 1)
-    .toList
   val visited = Array.fill(n + 1)(false)
   val queue = Queue.empty[Int]
   for (s <- starts) {
@@ -36,12 +36,25 @@ import scala.collection.mutable.Queue
   }
   while (!queue.isEmpty) {
     val x = queue.dequeue()
-    for (y <- graph(x)) {
-      if !visited(y) then {
-        visited(y) = true
-        queue.enqueue(y)
-      }
+    for { y <- graph(x) if !visited(y) } {
+      visited(y) = true
+      queue.enqueue(y)
     }
   }
 
   println(visited.count(_ == true))
+
+class FastScanner(in: java.io.InputStream):
+  private val reader = new BufferedReader(new InputStreamReader(in))
+  private var tokenizer: StringTokenizer = null
+
+  def next(): String =
+    while tokenizer == null || !tokenizer.hasMoreElements do
+      val line = reader.readLine()
+      if line == null then return null
+      tokenizer = new StringTokenizer(line)
+    tokenizer.nextToken()
+
+  def nextInt(): Int = next().toInt
+  def nextLong(): Long = next().toLong
+  def nextDouble(): Double = next().toDouble

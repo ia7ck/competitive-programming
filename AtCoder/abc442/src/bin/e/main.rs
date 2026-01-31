cd0f6dcd81f2b9e65941f3595e2963ed621232b0
ia7ck <23146842+ia7ck@users.noreply.github.com>
2026-01-31 12:24:58 +0900

    abc442e arg_cmp

--- AtCoder/abc442/src/bin/e/main.rs
+++ AtCoder/abc442/src/bin/e/main.rs
@@ -1,6 +1,8 @@
-use std::cmp::Ordering;
+use std::cmp::Reverse;
 
 use proconio::{input, marker::Usize1};
+
+use crate::arg_cmp::ArgCmp;
 
 fn main() {
     input! {
@@ -10,58 +12,83 @@ fn main() {
         ab: [(Usize1, Usize1); q],
     };
 
-    let mut ord = (0..n).collect::<Vec<_>>();
-    ord.sort_unstable_by_key(|&i| {
-        let (x, y) = xy[i];
-        x.pow(2) + y.pow(2)
-    });
-    // stable
-    ord.sort_by(|&i, &j| cmp(xy[i], xy[j]));
+    let clock_wise = |(x, y): (i64, i64)| Reverse(ArgCmp::new(x, y));
 
-    let mut inv_ord = vec![0; n];
-    for i in 0..n {
-        inv_ord[ord[i]] = i;
-    }
-
-    let mut eq_left = vec![0; n];
-    for i in 1..n {
-        eq_left[i] = match cmp(xy[ord[i - 1]], xy[ord[i]]) {
-            Ordering::Equal => eq_left[i - 1],
-            Ordering::Less => i,
-            _ => unreachable!(),
-        };
-    }
-    let mut eq_right = vec![0; n];
-    eq_right[n - 1] = n - 1;
-    for i in (0..(n - 1)).rev() {
-        eq_right[i] = match cmp(xy[ord[i]], xy[ord[i + 1]]) {
-            Ordering::Equal => eq_right[i + 1],
-            Ordering::Less => i,
-            _ => unreachable!(),
-        };
-    }
+    let mut xy_sorted = xy.clone();
+    xy_sorted.sort_by_key(|&p| clock_wise(p));
 
     for (a, b) in ab {
-        let ia = inv_ord[a];
-        let ib = inv_ord[b];
-        if cmp(xy[a], xy[b]).is_eq() {
-            let ans = eq_right[ia] - eq_left[ib] + 1;
-            println!("{}", ans);
-        } else if ia < ib {
-            let left = eq_right[ia] + 1;
-            let right = n - eq_left[ib];
-            let ans = left + right;
-            println!("{}", ans);
+        let p_a = xy[a];
+        let p_b = xy[b];
+        let i_a = xy_sorted.partition_point(|&p| clock_wise(p) < clock_wise(p_a));
+        let i_b = xy_sorted.partition_point(|&p| clock_wise(p) <= clock_wise(p_b));
+        let ans = if i_b > i_a {
+            i_b - i_a
         } else {
-            let ans = eq_right[ia] - eq_left[ib] + 1;
-            println!("{}", ans);
-        }
+            n - (i_a - i_b)
+        };
+
+        println!("{}", ans);
     }
 }
 
-// https://ngtkana.hatenablog.com/entry/2021/11/13/202103
-fn cmp((x1, y1): (i64, i64), (x2, y2): (i64, i64)) -> Ordering {
-    ((y1, x1) < (0, 0))
-        .cmp(&((y2, x2) < (0, 0)))
-        .then((x2 * y1).cmp(&(x1 * y2)))
+// Bundled
+#[rustfmt::skip]
+#[allow(unused)]
+mod arg_cmp {
+    use ::std::cmp::Ordering;
+
+    #[derive(Debug, PartialEq, Eq, Clone, Copy)]
+    pub struct ArgCmp((i64, i64));
+
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+    pub enum Quadrant {
+        First,
+        Second,
+        Third,
+        Fourth,
+    }
+
+    impl ArgCmp {
+        pub fn new(x: i64, y: i64) -> Self {
+            assert_ne!((x, y), (0, 0));
+            Self((x, y))
+        }
+
+        pub fn x(&self) -> i64 {
+            self.0.0
+        }
+
+        pub fn y(&self) -> i64 {
+            self.0.1
+        }
+
+        fn is_lower_half(&self) -> bool {
+            self.y() < 0 || (self.y() == 0 && self.x() < 0)
+        }
+
+        pub fn quadrant(&self) -> Quadrant {
+            match (self.is_lower_half(), self.x().cmp(&0)) {
+                (false, Ordering::Greater) => Quadrant::First,
+                (false, _) => Quadrant::Second,
+                (true, Ordering::Less) => Quadrant::Third,
+                (true, _) => Quadrant::Fourth,
+            }
+        }
+    }
+
+    impl Ord for ArgCmp {
+        fn cmp(&self, other: &Self) -> Ordering {
+            self.is_lower_half()
+                .cmp(&other.is_lower_half())
+                .then_with(||
+                (self.y() * other.x()).cmp(&(self.x() * other.y())))
+        }
+    }
+
+    impl PartialOrd for ArgCmp {
+        fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+            Some(self.cmp(other))
+        }
+    }
 }

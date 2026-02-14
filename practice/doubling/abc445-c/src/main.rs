b5f710a6808088fde544cb0998fd17322f0b79ce
ia7ck <23146842+ia7ck@users.noreply.github.com>
2026-02-15 02:58:00 +0900

    doubling

---
+++ practice/doubling/abc445-c/src/main.rs
@@ -0,0 +1,128 @@
+use proconio::{input, marker::Usize1};
+
+use crate::doubling::{Doubling, Transition};
+
+fn main() {
+    input! {
+        n: usize,
+        a: [Usize1; n],
+    };
+
+    let db = Doubling::new(n, n, |i| Transition::new(a[i], ()));
+    let mut ans = Vec::new();
+    for s in 0..n {
+        let last = db.fold(s, n, s, |_, t| t.next);
+        ans.push(last);
+    }
+
+    println!(
+        "{}",
+        ans.iter()
+            .map(|ans| (ans + 1).to_string())
+            .collect::<Vec<_>>()
+            .join(" ")
+    );
+}
+
+// Bundled
+#[rustfmt::skip]
+#[allow(unused)]
+mod doubling {
+    #[derive(Debug, Clone)]
+    pub struct Doubling<V> {
+        transitions: Vec<Transition<V>>,
+        n_state: usize,
+        max_steps: usize,
+        log2_max_steps: usize,
+    }
+
+    #[derive(Debug, Clone)]
+    pub struct Transition<V> {
+        pub next: usize,
+        pub value: V,
+    }
+
+    impl<V> Transition<V> {
+        pub fn new(next: usize, value: V) -> Self {
+            Self { next, value }
+        }
+    }
+
+    pub trait Value {
+        fn op(&self, other: &Self) -> Self;
+    }
+
+    impl Value for () {
+        fn op(&self, _other: &Self) -> Self {}
+    }
+
+    impl<V> Doubling<V>
+    where
+        V: Value,
+    {
+        pub fn new<F>(n_state: usize, max_steps: usize, step1: F) -> Self
+        where
+            F: Fn(usize) -> Transition<V>,
+        {
+            assert!(max_steps > 0);
+
+            let log2_max_steps = max_steps.ilog2() as usize;
+
+            let mut transitions = Vec::with_capacity(n_state * (log2_max_steps + 1));
+            for i in 0..n_state {
+                let t = step1(i);
+
+                assert!(t.next < n_state);
+
+                transitions.push(t);
+            }
+
+            for k in 1..=log2_max_steps {
+                let offset = n_state * (k - 1);
+                for i in 0..n_state {
+                    let t1 = &transitions[offset + i];
+                    let t2 = &transitions[offset + t1.next];
+                    transitions.push(Transition {
+                        next: t2.next,
+                        value: t1.value.op(&t2.value),
+                    });
+                }
+            }
+
+            Self {
+                transitions,
+                n_state,
+                max_steps,
+                log2_max_steps,
+            }
+        }
+
+        pub fn get(&self, start: usize, k: usize) -> &Transition<V> {
+            assert!(start < self.n_state);
+            assert!(k <= self.log2_max_steps);
+
+            let offset = self.n_state * k;
+            &self.transitions[offset + start]
+        }
+
+        pub fn fold<A, F>(&self, start: usize, step: usize, init: A, mut f: F) -> A
+        where
+            F: FnMut(A, &Transition<V>) -> A,
+        {
+            assert!(start < self.n_state);
+            assert!(step <= self.max_steps);
+
+            let mut i = start;
+            let mut acc = init;
+            for k in 0..=self.log2_max_steps {
+                if step >> k & 1 == 1 {
+                    let offset = self.n_state * k;
+                    let t = &self.transitions[offset + i];
+                    (i, acc) = (t.next, f(acc, t));
+                }
+            }
+
+            acc
+        }
+    }
+}

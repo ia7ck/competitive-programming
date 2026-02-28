4e7e72e614669421d2871666cf6c01f69b51631e
ia7ck <23146842+ia7ck@users.noreply.github.com>
2026-03-01 03:15:25 +0900

    abc447-f2

---
+++ AtCoder/abc447/src/bin/f2/main.rs
@@ -0,0 +1,165 @@
+use proconio::{input, marker::Usize1};
+
+use crate::re_rooting_dp::re_rooting_dp;
+
+fn main() {
+    input! {
+        q: usize,
+    };
+
+    for _ in 0..q {
+        input! {
+            n: usize,
+            edges: [(Usize1, Usize1); n - 1],
+        };
+
+        solve(n, edges);
+    }
+}
+
+fn solve(n: usize, edges: Vec<(usize, usize)>) {
+    let mut deg = vec![0; n];
+    for &(a, b) in &edges {
+        deg[a] += 1;
+        deg[b] += 1;
+    }
+
+    #[derive(Debug, Clone)]
+    struct V {
+        i: usize,
+        start_k: usize,
+        mid_k: usize,
+        end_k: usize,
+    }
+    impl V {
+        fn new(i: usize, start_k: usize, mid_k: usize, end_k: usize) -> Self {
+            Self {
+                i,
+                start_k,
+                mid_k,
+                end_k,
+            }
+        }
+    }
+
+    let edges = edges
+        .into_iter()
+        .map(|(a, b)| (a, b, ()))
+        .collect::<Vec<_>>();
+    let dp = re_rooting_dp(
+        n,
+        &edges,
+        |i| V::new(i, 0, 0, 0),
+        |p, ch, _e| {
+            let &V {
+                i,
+                mut start_k,
+                mut mid_k,
+                mut end_k,
+            } = p;
+
+            if deg[i] >= 3 {
+                start_k = start_k.max(1);
+            }
+            if deg[i] >= 4 {
+                mid_k = mid_k.max(1 + ch.start_k.max(ch.mid_k));
+            }
+            if deg[i] >= 3 {
+                end_k = end_k.max(1 + ch.start_k.max(ch.mid_k));
+            }
+
+            V::new(i, start_k, mid_k, end_k)
+        },
+    );
+
+    // n >= 3
+    let mut ans = 1;
+    for v in dp {
+        ans = ans.max(v.start_k).max(v.mid_k).max(v.end_k);
+    }
+
+    println!("{}", ans);
+}
+
+// Bundled
+#[rustfmt::skip]
+#[allow(unused)]
+mod re_rooting_dp {
+    pub fn re_rooting_dp<E, V, F, G>(n: usize, edges: &[(usize, usize, E)], new: F, fold: G) -> Vec<V>
+    where
+        V: Clone,
+        F: Fn(usize) -> V,
+        G: Fn(&V, &V, &E) -> V,
+    {
+        if n == 0 {
+            return Vec::new();
+        }
+
+        let (g, pre_order) = {
+            let mut g = vec![vec![]; n];
+            for (u, v, e) in edges {
+                g[*u].push((*v, e));
+                g[*v].push((*u, e));
+            }
+            let mut ord = Vec::with_capacity(n);
+            let mut stack = vec![(0, usize::MAX)];
+            while let Some((i, p)) = stack.pop() {
+                ord.push(i);
+                g[i].retain(|&(j, _)| j != p);
+                for &(j, _) in &g[i] {
+                    stack.push((j, i));
+                }
+            }
+            (g, ord)
+        };
+
+        let dp_sub = {
+            let mut dp_sub = (0..n).map(&new).collect::<Vec<_>>();
+            for &i in pre_order.iter().rev() {
+                for &(j, e) in &g[i] {
+                    dp_sub[i] = fold(&dp_sub[i], &dp_sub[j], e);
+                }
+            }
+            dp_sub
+        };
+
+        let mut dp_p = (0..n).map(&new).collect::<Vec<_>>();
+        for i in pre_order {
+            apply(dp_p[i].clone(), &g[i], &fold, &dp_sub, &mut dp_p);
+        }
+
+        dp_p.into_iter()
+            .enumerate()
+            .map(|(i, dp_p)| {
+                g[i].iter()
+                    .fold(dp_p, |acc, &(j, e)| fold(&acc, &dp_sub[j], e))
+            })
+            .collect::<Vec<_>>()
+    }
+
+    fn apply<E, V, G>(acc: V, children: &[(usize, &E)], fold: &G, dp_sub: &Vec<V>, dp_p: &mut Vec<V>)
+    where
+        V: Clone,
+        G: Fn(&V, &V, &E) -> V,
+    {
+        if children.is_empty() {
+            return;
+        }
+
+        if children.len() == 1 {
+            let (j, e) = children[0];
+            dp_p[j] = fold(&dp_p[j], &acc, e);
+            return;
+        }
+
+        let (left, right) = children.split_at(children.len() / 2);
+        let left_acc = left
+            .iter()
+            .fold(acc.clone(), |acc, &(j, e)| fold(&acc, &dp_sub[j], e));
+        let right_acc = right
+            .iter()
+            .fold(acc, |acc, &(j, e)| fold(&acc, &dp_sub[j], e));
+        apply(left_acc, right, fold, dp_sub, dp_p);
+        apply(right_acc, left, fold, dp_sub, dp_p);
+    }
+}
